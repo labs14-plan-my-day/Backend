9dbd7775cbe6edc95974423be8cc2643acf09143
tomexar <tommyexar@gmail.com>
2019-07-12 12:17:15 -0600

    started to use Auth0

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-const server = require('./server.js');
-const port = process.env.PORT || 1100;
+var express = require('express');
+const server = require('./api/server.js');
+var app = express();
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
+var port = process.env.PORT || 8080;
+
+
+var jwtCheck = jwt({
+      secret: jwks.expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: 'https://plan-my-day.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'https://plan-my-day',
+    issuer: 'https://plan-my-day.auth0.com/',
+    algorithms: ['RS256']
+});
+
+server.use(jwtCheck);
+
+server.get('/authorized', (req, res)=>{
+    res.send('Secured Resource');
+});
+
 server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
+
