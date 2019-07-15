a67830e8873a120d57b64a8814e33b75105b3263
tomexar <tommyexar@gmail.com>
2019-07-15 15:56:42 -0600

    finished login and added secrets

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const Users = require('./auth-model')
-const jwtCheck = require('./jwtCheck')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
+const secret = require('../secret.js');
 
 router.get('/', (req, res) => {
     Users.find()
@@ -23,4 +23,35 @@ router.post('/register', (req, res) => {
         })
 })
 
+router.post('/login', (req, res)=> {
+    let { username, password} = req.body
+    console.log({username})
+    Users.findBy( username )
+    .first()
+    .then(user =>{
+        console.log(user)
+        if(user && bcrypt.compareSync(password, user.password)){
+            let token = generateToken(user.username)
+            res.send({message: "welcome!", token: token})
+        } else {
+            res.status(401).json({ message: 'invalid'})
+        }
+    }).catch(err =>{
+        console.log({error:err.message})
+        res.status(500).json(err)
+    })
+})
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username
+    };
+
+    const options = {
+        expiresIn: '1d'
+    };
+
+    return jwt.sign(payload, secret.jwtSecret, options);
+}
 module.exports = router;
