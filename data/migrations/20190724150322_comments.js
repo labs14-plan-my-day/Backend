4e4d1e80a6c5e5f08650f18789164acbade941e2
tomexar <tommyexar@gmail.com>
2019-07-24 16:14:38 -0600

    added task, comments server, added routers and models for tasks, all CRUD for tasks are working, get for comments is working

---
+++ data/migrations/20190724150322_comments.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('comments', tbl =>{
+        tbl.increments();
+        tbl
+          .integer('task_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('tasks')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+
+        tbl.string('commentor', 200).notNullable();
+        tbl.text('comment', 300).notNullable();
+        tbl.date("date", 30).defaultTo(knex.fn.now());
+    })
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('comments')
+  };
+  
